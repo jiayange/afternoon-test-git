c1e83f9f94e8c8654d5cfd4450d9297734fd76b3
jiayange <475345457@qq.com>
2019-08-31 15:28:58 +0800

    添加box

--- app1/src/views/About.vue
+++ app1/src/views/About.vue
@@ -2,5 +2,11 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h2>修改</h2>
+    <div class="box"></div>
   </div>
 </template>
+<style scoped>
+   .box{
+     width: 100px;height: 100px;border-radius: 50%;background:coral;
+   }
+</style>
